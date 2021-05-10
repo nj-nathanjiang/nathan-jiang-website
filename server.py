77a1965dcd701e0285d654dda2e2900f4e8ebb79
Nathan Jiang <nathanj0601@gmail.com>
2021-05-10 16:27:17 -0700

    Add Custom 404 Page

--- server.py
+++ server.py
@@ -603,5 +603,10 @@ def load_login_user(user_id):
     return User.query.get(int(user_id))
 
 
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('404.html'), 404
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
